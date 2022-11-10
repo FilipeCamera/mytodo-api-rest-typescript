56d016cd8bc866e283a246ff7cdb168bd6f3479f
FilipeCamera <filipecameracastro@gmail.com>
2022-11-10 18:27:33 -0300

    refatorando a rota de user

---
+++ src/routes/user.router.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { body } from "express-validator";
+import UserController from "../controllers/user.controller";
+
+import MyToDoDataSource from "../database";
+import Role from "../models/role";
+import User from "../models/user";
+
+const userRoutes = express.Router();
+
+const userRepository = MyToDoDataSource.getRepository(User);
+const roleRepository = MyToDoDataSource.getRepository(Role);
+
+const userController = new UserController(userRepository, roleRepository);
+
+userRoutes.post(
+  "/users",
+  body("email").isEmail().notEmpty().exists(),
+  body("password").notEmpty().exists().isLength({ min: 6 }),
+  userController.create
+);
+
+export default userRoutes;
